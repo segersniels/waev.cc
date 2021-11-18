849a4bcd4ba3a96144fbaa7dd657d0fb597daf9f
segersniels <segers.n@hotmail.com>
2021-11-18 16:29:55 +0100

    :fire: Remove unused color addition

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        purple: '#6236FF',
         gradient: {
           from: 'rgb(236, 63, 251)',
           to: 'rgba(252, 229, 70, 1)',
