4f786f981457b9a7dc4f4598a0093aa288fc3475
segersniels <segers.n@hotmail.com>
2021-11-18 17:41:12 +0100

    :wrench: Add screen ignore rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'at-rule-no-unknown': [
       true,
       {
-        ignoreAtRules: ['tailwind'],
+        ignoreAtRules: ['tailwind', 'screen'],
       },
     ],
     'declaration-block-trailing-semicolon': null,
