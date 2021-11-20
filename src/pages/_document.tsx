01335c2a0ff71626c379d8082b5efc7383b7bddf
segersniels <segers.n@hotmail.com>
2021-11-20 14:22:20 +0100

    :truck: Move custom font to document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:300,400,500,600,700&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
