5a8a123c137e86d7445a5ac79f4967aa72bf38a2
segersniels <segers.n@hotmail.com>
2021-11-20 14:32:45 +0100

    :ambulance: Font import is not handled correctly

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:300,400,500,600,700&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
